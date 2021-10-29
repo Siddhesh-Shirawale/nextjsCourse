0b67209d64a091c47a901d377357a5ff21245be6
Siddhesh-Shirawale <siddhesh.shirawale@gmail.com>
2021-10-29 16:41:53 +0530

    Header and footer

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import Layout from "../components/Layout.js";
+
 export default function AboutPage() {
    return (
-      <div>
+      <Layout title="About DJ Events">
          <h1>About</h1>
          <p>This is an App to find the latest DJ and Musical events</p>
          <p>Version: 1.0.0</p>
-      </div>
+      </Layout>
    );
 }
